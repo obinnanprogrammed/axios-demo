dd670712da52779dcc5c399a14acb7f0187e5cae
obinnanprogammed <obinnanwakwue10@gmail.com>
2025-04-06 15:34:17 -0500

    actual backend fix

--- axiosdemo-backend/src/main/java/com/example/axiosdemo_backend/BackendController.java
+++ axiosdemo-backend/src/main/java/com/example/axiosdemo_backend/BackendController.java
@@ -1,12 +1,16 @@
 package com.example.axiosdemo_backend;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins="http://localhost:5173")
 public class BackendController {
     @RequestMapping("/hello")
-    public String index() {
-        return "Hello there from Spring Boot!";
+    public String index(@RequestBody String name) {
+        String trim = name.substring(0, name.length()-1);
+        return "Hello " + trim + " from Spring Boot!";
     }
 }

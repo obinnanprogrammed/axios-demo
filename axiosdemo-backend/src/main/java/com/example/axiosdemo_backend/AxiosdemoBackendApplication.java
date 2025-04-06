513a9e30ef165b4eb186933d914312dc84a450d5
obinnanprogammed <obinnanwakwue10@gmail.com>
2025-04-06 14:39:40 -0500

    files

---
+++ axiosdemo-backend/src/main/java/com/example/axiosdemo_backend/AxiosdemoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.axiosdemo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxiosdemoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxiosdemoBackendApplication.class, args);
+	}
+
+}
---
+++ axiosdemo-backend/src/test/java/com/example/axiosdemo_backend/AxiosdemoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.axiosdemo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxiosdemoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
